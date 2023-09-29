0d4f2d6bd88093211850302731a7c5f72739716a
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2023-09-29 13:23:59 +0530

    Initial commit

---
+++ 1.c
@@ -0,0 +1,20 @@
+/*Program in c to print a pattern of
+ * 1
+ * 22
+ * 333
+ * 4444
+ * 55555*/
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
