54f48fddc8cdb3d660cded6df54d0a757a1105ad
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2023-10-09 21:46:08 +0530

    assignments

--- hourglass_star.c
+++ hourglass_star.c
@@ -2,35 +2,34 @@
 #include <stdio.h>
 int main()
 {
-    int i,j,lim,t; // i = outer loop , j= inner loop 1 ,k= inner loop 2, l = limit of pattern ,t= total lines
+    int i,j,lim; // i = outer loop , j= inner loop 1 ,k= inner loop 2, l = limit of pattern
     printf("ENTER THE LIMIT OF PATTERN >> ");
     scanf("%d",&lim);
-    t=(lim*2)-1;
-    for (i=1;i<=t;i++)
+    printf("\n");
+    for (i=0;i<=lim;i++)
     {
+        for (j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+
+        for (j = lim - i; j >= 0; j--)
+        {
+            printf("* ");
+        }
         printf("\n");
-        if(i<=lim)
-        {
-            for (j = 0; j <= i; j++) {
-                printf(" ");
-            }
-
-            for (j = lim - i; j >= 0; j--) {
-                printf("* ");
-            }
-        }
-        else
-        {
-            for (j = (i/2); j >= 0; j--)
-            {
-                printf(" ");
-            }
-            for (j = t-1; j >= i; j--)
-            {
-                printf("* ");
-            }
-        }
-
     }
+    for(i=1;i<=lim;i++)
+    {
 
+        for (j = lim; j>i; j--)
+        {
+            printf(" ");
+        }
+        for (j = 0; j <=i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
 }
