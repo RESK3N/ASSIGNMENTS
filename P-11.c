161b96fcaf5b479d0b2c83cb7fa51f2e1c4897a1
PRITAM MONDAL <131168290+RESK3N@users.noreply.github.com>
2023-11-18 21:05:16 +0530

    assignment

--- P-11.c
+++ P-11.c
@@ -2,11 +2,12 @@
 #include<stdio.h>
 int main()
 {
-    float rupees;
+    float r;
     printf("Enter the amount in Rupees:\n");
-    scanf("%f",&rupees);
-    printf("Amount in Dollar:%f\n",rupees*0.012);
-    printf("Amount in Euro:%f\n",rupees*0.011);
-    printf("Amount in Yen:%f\n",rupees*1.77);
-    printf("Amount in Rubel:%f\n",rupees*1.16);
+    scanf("%f",&r);
+    printf("Amount in Dollar:%f\n",r*0.012);
+    printf("Amount in Euro:%f\n",r*0.011);
+    printf("Amount in pound :%f\n",r*0.0096);
+    printf("Amount in Yen:%f\n",r*1.795);
+    printf("Amount in Rubel:%f\n",r*1.07);
 }
